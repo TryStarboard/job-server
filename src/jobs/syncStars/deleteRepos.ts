8f113428150ca7d24582b278fe2e50793966ef52
Daiwei <daiweilu123@gmail.com>
2016-07-11 13:33:44 -0400

    Fix delete repos return data

--- src/jobs/syncStars/deleteRepos.ts
+++ src/jobs/syncStars/deleteRepos.ts
@@ -1,12 +1,19 @@
-import {Repo} from '../../models';
+import {UserInstance} from '@starboard/models';
+import {Repo, sequelize} from '../../models';
 
-export default async function (repoIds: string[]) {
-  await Repo.destroy({
-    where: {
-      id: {
-        $notIn: repoIds
-      }
-    }
+export default async function (user: UserInstance, repoIds: string[]) {
+  return await sequelize.transaction(async function (t) {
+    const repos = await user.getRepos({
+      where: {
+        id: {
+          $notIn: repoIds
+        }
+      },
+      transaction: t,
+    })
+
+    await Promise.all(repos.map(r => r.destroy({transaction: t})));
+
+    return repos.map(r => r.id);
   });
-  return repoIds;
 };
--- src/jobs/syncStars/index.ts
+++ src/jobs/syncStars/index.ts
@@ -1,15 +1,39 @@
+import {Observable, Subscriber} from 'rxjs';
 import {User} from '../../models';
 import createDataSource from './createDataSource';
 import saveData from './saveData';
 import deleteRepos from './deleteRepos';
 
+interface SyncStarEvent {
+  type: string;
+  data: any;
+}
+
 export default async function (userId: string) {
   const user = await User.findById(userId);
   const dataSource = createDataSource(user);
-  return dataSource
-    .flatMap<string[]>((data) => saveData(user, data.repos))
-    .do((ids) => console.log('-----------------> ', ids.length))
-    .reduce<string[]>((touchedRepoIds, repoIds) => touchedRepoIds.concat(repoIds), [])
-    .do((ids) => console.log('-----------------> all ', ids.length))
-    .flatMap<string[]>(deleteRepos);
+
+  return new Observable<SyncStarEvent>((subscriber: Subscriber<SyncStarEvent>) => {
+    const subscription = dataSource
+      .flatMap<string[]>((data) => saveData(user, data.repos))
+      .do((repoIds) => subscriber.next({
+        type: 'REPOS_TOUCHED',
+        data: repoIds,
+      }))
+      .reduce<string[]>((touchedRepoIds, repoIds) => touchedRepoIds.concat(repoIds), [])
+      .flatMap<string[]>((repoIds) => deleteRepos(user, repoIds))
+      .do((repoIds) => subscriber.next({
+        type: 'REPOS_DELETED',
+        data: repoIds,
+      }))
+      .subscribe(
+        null,
+        (err) => subscriber.error(err),
+        () => subscriber.complete()
+      );
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  });
 };
